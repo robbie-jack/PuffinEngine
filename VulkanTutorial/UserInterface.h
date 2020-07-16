ce1d77ee07683d5aaf10dd5042eb214ed904a84c
robbie-jack <35919121+robbie-jack@users.noreply.github.com>
2020-07-16 12:43:47 +0100

    Switched to ImGui Docking Branch

--- VulkanTutorial/UserInterface.h
+++ VulkanTutorial/UserInterface.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_vulkan.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_vulkan.h"
 
 #include "InputManager.h"
 
--- VulkanTutorial/VulkanRenderer.h
+++ VulkanTutorial/VulkanRenderer.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_vulkan.h"
+//#include "imgui.h"
+//#include "examples/imgui_impl_glfw.h"
+//#include "examples/imgui_impl_vulkan.h"
 
 #include "UserInterface.h"
 #include "InputManager.h"
