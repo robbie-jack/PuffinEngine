7e5dc8a702bc153e1af96347246418923fdc7fd3
Robbie Jack <r0bsterj29@gmail.com>
2024-03-03 16:59:42 +0000

    All assets in content directory are now displayed in a tree, with working right click context menus.

--- include/UI/Editor/Windows/UIContentBrowser.h
+++ include/UI/Editor/Windows/UIContentBrowser.h
@@ -15,5 +15,30 @@ namespace puffin::ui
 		~UIContentBrowser() override {}
 
 		void draw(double dt) override;
+
+
+	private:
+
+		enum class EntryType
+		{
+			Directory = 0,
+			File = 1
+		};
+
+		struct ContentEntryNode
+		{
+			std::string displayName;
+			EntryType entryType;
+			int index = 0;
+
+			std::vector<ContentEntryNode> childNodes;
+		};
+
+		int mNodeCounter = 0;
+		int mSelectedNode = 0;
+
+		void GenerateChildEntryNodes(const fs::directory_entry& dirEntry, ContentEntryNode& rootNode);
+		void GenerateUIForNode(const ContentEntryNode& node, const ImGuiTreeNodeFlags& baseFlags);
+
 	};
 }
--- src/UI/Editor/Windows/UIContentBrowser.cpp
+++ src/UI/Editor/Windows/UIContentBrowser.cpp
@@ -16,18 +16,105 @@ namespace puffin::ui
 
 			begin(mWindowName);
 
+			ImVec2 listBoxSize = ImGui::GetWindowSize();
+			listBoxSize.y -= 20.0f;
+
 			const fs::path contentPath = assets::AssetRegistry::get()->contentRoot();
 
 			if (exists(contentPath))
 			{
-				for (auto const& dirEntry : fs::directory_iterator(contentPath))
+				if (ImGui::BeginListBox("##ContentList", listBoxSize))
 				{
-					std::string pathName = dirEntry.path().string();
-					ImGui::Text("%s", pathName.c_str());
+					constexpr ImGuiTreeNodeFlags baseFlags = ImGuiTreeNodeFlags_OpenOnArrow
+						| ImGuiTreeNodeFlags_OpenOnDoubleClick
+						| ImGuiTreeNodeFlags_SpanAvailWidth;
+
+					mNodeCounter = 0;
+
+					ContentEntryNode rootNode;
+					rootNode.index = mNodeCounter;
+
+					mNodeCounter++;
+
+					GenerateChildEntryNodes(fs::directory_entry(contentPath), rootNode);
+
+					for (const auto& node : rootNode.childNodes)
+					{
+						GenerateUIForNode(node, baseFlags);
+					}
+
+					ImGui::EndListBox();
 				}
 			}
 
 			end();
+		}
+	}
+
+	void UIContentBrowser::GenerateChildEntryNodes(const fs::directory_entry& dirEntry, ContentEntryNode& rootNode)
+	{
+		fs::path path = dirEntry.path();
+
+		for (auto const& entry : fs::directory_iterator(path))
+		{
+			ContentEntryNode node;
+			node.displayName = entry.path().filename().string();
+			node.index = mNodeCounter;
+
+			mNodeCounter++;
+
+			if (entry.is_directory())
+			{
+				node.entryType = EntryType::Directory;
+
+				GenerateChildEntryNodes(entry, node);
+			}
+			else if (entry.is_regular_file())
+			{
+				node.entryType = EntryType::File;
+			}
+
+			rootNode.childNodes.push_back(node);
+		}
+	}
+
+	void UIContentBrowser::GenerateUIForNode(const ContentEntryNode& node, const ImGuiTreeNodeFlags& baseFlags)
+	{
+		const ImGuiTreeNodeFlags treeFlags = baseFlags;
+
+		switch(node.entryType)
+		{
+		case EntryType::Directory:
+
+			if (ImGui::TreeNodeEx(node.displayName.c_str(), treeFlags))
+			{
+				for (const auto& childNode : node.childNodes)
+				{
+					GenerateUIForNode(childNode, baseFlags);
+				}
+
+				ImGui::TreePop();
+			}
+
+			break;
+		case EntryType::File:
+
+			int idx = node.index;
+
+			ImGui::Dummy(ImVec2(10.0f, 0.0f)); ImGui::SameLine();
+			if (ImGui::Selectable(node.displayName.c_str(), mSelectedNode == idx))
+			{
+				mSelectedNode = idx;
+			}
+			if (ImGui::BeginPopupContextItem(node.displayName.c_str()))
+			{
+				mSelectedNode = idx;
+
+				ImGui::Text("Popup for %s", node.displayName.c_str());
+				ImGui::EndPopup();
+			}
+
+			break;
 		}
 	}
 }
